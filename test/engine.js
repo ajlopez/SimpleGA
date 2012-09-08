6558e6047065028faa36c3b15e2520197354c0f6
ajlopez <webmaster@ajlopez.com>
2012-09-08 19:36:37 -0300

    Mutators, with non deterministic test

--- test/engine.js
+++ test/engine.js
@@ -6,7 +6,7 @@ var engine = new simplega.Engine();
 
 assert.ok(engine);
 
-var population = generateGenotypes(10);
+var population = generateGenotypes(10, 1);
 
 engine.setPopulation(population);
 
@@ -16,18 +16,78 @@ assert.ok(result);
 assert.ok(Array.isArray(result));
 assert.equal(10, result.length);
 
-function generateGenotypes(n)
+population = [];
+population.push(new SimpleGenotype(1));
+population.push(new SimpleGenotype(2));
+
+engine.setPopulation(population);
+
+var result = engine.nextPopulation();
+
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.ok(result.length >= 1);
+
+var mutator = new SimpleMutator();
+
+var mutators = [ mutator, mutator, mutator ];
+
+engine.setMutators(mutators);
+
+result = engine.nextPopulation();
+
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.ok(result.length >= 1);
+
+engine.setPopulation(generateGenotypes(10, 1));
+
+for (var k = 1; k <= 10; k++)
+{
+    result = engine.nextPopulation();
+    engine.setPopulation(result);
+}
+
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.ok(result.length >= 1);
+assert.ok(getMaxValue(result) >= 1);
+
+function generateGenotypes(n, value)
 {
     var genotypes = [];
     
     for (var k = 0; k < n; k++)
-        genotypes.push(new SimpleGenotype());
+        genotypes.push(new SimpleGenotype(value));
         
     return genotypes;
 }
 
-function SimpleGenotype()
+function SimpleGenotype(value)
 {
-    this.evaluate = function() { return 1; }
+    this.evaluate = function() { return value; }
 }
 
+function SimpleMutator()
+{
+    this.mutate = function(genotype) {
+        return new SimpleGenotype(genotype.evaluate() + Math.random() - 0.5);
+    }
+}
+
+function getMaxValue(population) {
+    var l = population.length;
+    
+    if (l == 0)
+        return 0;
+        
+    var max = population[0].evaluate();
+    
+    for (k = 1; k < l; k++) {
+        var value = population[k].evaluate();
+        if (value > max)
+            max = value;
+    }
+    
+    return max;
+}
