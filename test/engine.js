8d776523633d53185109ab06ae0cedf7c9fd1eac
ajlopez <webmaster@ajlopez.com>
2012-09-08 11:47:43 -0300

    simple nextPopulation, with test

--- test/engine.js
+++ test/engine.js
@@ -6,3 +6,28 @@ var engine = new simplega.Engine();
 
 assert.ok(engine);
 
+var population = generateGenotypes(10);
+
+engine.setPopulation(population);
+
+var result = engine.nextPopulation();
+
+assert.ok(result);
+assert.ok(Array.isArray(result));
+assert.equal(10, result.length);
+
+function generateGenotypes(n)
+{
+    var genotypes = [];
+    
+    for (var k = 0; k < n; k++)
+        genotypes.push(new SimpleGenotype());
+        
+    return genotypes;
+}
+
+function SimpleGenotype()
+{
+    this.evaluate = function() { return 1; }
+}
+
