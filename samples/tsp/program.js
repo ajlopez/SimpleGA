a2f1ec7229dd01614b8091387bc28f248512e991
ajlopez <webmaster@ajlopez.com>
2012-09-09 07:02:50 -0300

    TSP sample

--- samples/tsp/program.js
+++ samples/tsp/program.js
@@ -53,23 +53,51 @@ function Genotype(points, maxlength, values)
         if (value)
             return value;
             
-        value = 0;
+        value = maxlength;
         
-        var x = points[0].x;
-        var y = points[0].y;
+        var x = points[values[0]].x;
+        var y = points[values[0]].y;
         
         for (var k = 1; k < n; k++)
         {
-            var x2 = points[k].x;
-            var y2 = points[k].y;
+            var x2 = points[values[k]].x;
+            var y2 = points[values[k]].y;
             
-            value += (x-x2) * (x-x2) + (y-y2)*(y-y2);
+            value -= (x-x2) * (x-x2) + (y-y2)*(y-y2);
             
             x = x2;
             y = y2;
         }
         
-        return maxlength - value;
+        return value;
+    }
+    
+    this.getValues = function() { return values; }
+    
+    this.clone = function(newvalues) {
+        return new Genotype(points, maxlength, newvalues);
+    }
+}
+
+function Mutator() {
+    this.mutate = function(genotype) {
+        if (Math.random() >= 0.2)
+            return genotype;
+            
+        var values = genotype.getValues().slice(0);
+        var l = values.length;
+        
+        var pos1 = Math.floor(Math.random() * l);
+        var pos2 = (pos1 + 1) % l;
+        
+/*        if (Math.random() >= 0.8)
+            pos2 = Math.floor(Math.random() * l);*/
+        
+        var value = values[pos1];
+        values[pos1] = values[pos2];
+        values[pos2] = value;
+        
+        return genotype.clone(values);
     }
 }
 
@@ -78,19 +106,31 @@ var maxlength = 3 * 4 * (3*3 + 4*4);
 
 var population = [];
 
-for (var k = 1; k < 50; k++)
+for (var k = 1; k < 5000; k++)
     population.push(new Genotype(points, maxlength));
     
 var engine = new simplega.Engine();
 
 engine.setPopulation(population);
+engine.setMutators([new Mutator()]);
 
-console.log(simplega.getBestValue(population));
-
-for (k = 1; k < 30; k++)
+for (k = 1; k < 500; k++)
 {
     population = engine.nextPopulation();
-    console.log(population.length);
-    console.log(simplega.getBestValue(population));
+    var bestvalue = simplega.getBestValue(population);
+    var bestpath = maxlength - bestvalue;
+    var l = population.length;
+    console.log(l + ': ' + bestpath);
     engine.setPopulation(population);
+}
+
+function dump(points, values)
+{
+    var l = values.length;
+    var result = new Array(l);
+    
+    for (var k =0; k < l; k++)
+        result[k] = [ points[values[k]].x, points[values[k]].y ];
+        
+    console.log(result);
 }
