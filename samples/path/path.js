050ad3d042ff3d07ece9c152706aa037bc54fb2a
ajlopez <webmaster@ajlopez.com>
2014-11-22 11:23:45 -0300

    Fill world first test

--- samples/path/path.js
+++ samples/path/path.js
@@ -1,11 +1,23 @@
 
-var Path = (function () {
+var Path = (function () {    
     function World(width, height) {
+        var values = [];
+        
+        for (var x = 0; x < width; x++)
+            for (var y = 0; y < height; y++)
+                values[y * width + x] = false;
+        
         this.width = function () { return width; }
         
         this.height = function () { return height; }
         
-        this.get = function (x, y) { return false; }
+        this.get = function (x, y) { return values[y * width + x]; }
+        
+        this.fill = function (ratio) {
+            for (var x = 0; x < width; x++)
+                for (var y = 0; y < height; y++)
+                    values[y * width + x] = Math.random() <= ratio;
+        }
     }
     
     return {
--- samples/path/test/world.js
+++ samples/path/test/world.js
@@ -13,3 +13,18 @@ exports['create empty world'] = function (test) {
             test.ok(!world.get(x, y));
 };
 
+exports['fill world ramdomly'] = function (test) {
+    var world = path.createWorld(10, 10);
+    
+    world.fill(0.5);
+    
+    var count = 0;
+    
+    for (var x = 0; x < 10; x++)
+        for (var y = 0; y < 10; y++)
+            if (world.get(x, y))
+                count++;
+                
+    test.ok(count > 5);
+};
+
