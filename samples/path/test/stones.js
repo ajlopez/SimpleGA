4a0506c46d45a661b56d204e33c9c25501c436f0
ajlopez <webmaster@ajlopez.com>
2014-11-22 12:22:54 -0300

    Stones additional diagonal path test, in free world

--- samples/path/test/stones.js
+++ samples/path/test/stones.js
@@ -18,3 +18,12 @@ exports['vertical line free world'] = function (test) {
     test.ok(Array.isArray(stones));
     test.equal(stones.length, 0);
 };
+
+exports['diagonal line free world'] = function (test) {
+    var world = path.createWorld(10, 10);
+    var stones = world.stones({ x: 0, y: 0 }, { x: 6, y: 6 });
+    
+    test.ok(stones);
+    test.ok(Array.isArray(stones));
+    test.equal(stones.length, 0);
+};
