044109be8cbe29d62954845d04fc8b00b2dd2a3e
ajlopez <webmaster@ajlopez.com>
2014-11-22 14:51:30 -0300

    Additional test two stones

--- samples/path/test/stones.js
+++ samples/path/test/stones.js
@@ -82,3 +82,19 @@ exports['inverse vertical line one stone'] = function (test) {
     test.equal(stones[0].y, 3);
 };
 
+exports['horizontal line two stone'] = function (test) {
+    var world = path.createWorld(10, 10);
+    
+    world.set(3, 0, true);    
+    world.set(4, 0, true);    
+    
+    var stones = world.stones({ x: 0, y: 0 }, { x: 6, y: 0 });
+
+    test.ok(stones);
+    test.ok(Array.isArray(stones));
+    test.equal(stones.length, 2);
+    test.equal(stones[0].x, 3);
+    test.equal(stones[0].y, 0);
+    test.equal(stones[1].x, 4);
+    test.equal(stones[1].y, 0);
+};
