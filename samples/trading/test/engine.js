802397dd08fa21d19348c69e020faa59b11012f0
ajlopez <webmaster@ajlopez.com>
2015-12-08 19:18:09 -0300

    Run generation with up and down series

--- samples/trading/test/engine.js
+++ samples/trading/test/engine.js
@@ -57,6 +57,41 @@ exports['run generation'] = function (test) {
     test.ok(best.evaluate() >= 1000);
 };
 
+exports['run generation with up and down series'] = function (test) {
+    var series = [
+        { amount: 1000, values: [ 1, 2, 3, 4, 5, 4, 3, 2, 1 ] }
+    ];
+    
+    var population = trading.population(1000, series);
+    var mutators = [ trading.mutator() ];
+    
+    var engine = new simplega.Engine();
+    engine.setPopulation(population);
+    engine.setMutators(mutators);
+    
+    var newpopulation = engine.nextPopulation();
+    
+    test.ok(newpopulation);
+    test.ok(Array.isArray(newpopulation));
+    test.ok(newpopulation.length);
+    
+    newpopulation.forEach(function (trader) {
+        test.ok(trader);
+        test.ok(trader.genes);
+        test.ok(Array.isArray(trader.genes));
+        test.ok(trader.genes.length);
+        test.strictEqual(trader.series, series);
+    });
+    
+    var best = simplega.getBestGenotype(newpopulation);
+    
+    test.ok(best);
+    console.log('best trader');
+    console.dir(best);
+    console.log('best value', best.evaluate());
+    test.ok(best.evaluate() >= 1000);
+};
+
 exports['run one hundred generations'] = function (test) {
     var series = [
         { amount: 1000, values: [ 1, 2, 3, 4 ] }
