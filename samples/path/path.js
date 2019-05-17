0917de6f5428d618c78b7b9ffbe6f1cc5bca56d1
ajlopez <webmaster@ajlopez.com>
2019-05-17 16:30:28 -0300

    Refactor path sample, WIP

--- samples/path/path.js
+++ samples/path/path.js
@@ -2,12 +2,12 @@
 var Path = (function () {    
     function Mutator() {
         this.mutate = function (genotype) {
-            var path = genotype.path();
-            var world = genotype.world();
-            var width = world.width();
-            var height = world.height();
+            const path = genotype.path();
+            const world = genotype.world();
+            const width = world.width();
+            const height = world.height();
             
-            var dice = Math.floor(Math.random() * 1000);
+            const dice = Math.floor(Math.random() * 1000);
             
             if (dice % 4 == 0)
                 return new Genotype(world, insertPoint(path, width, height));
@@ -22,23 +22,27 @@ var Path = (function () {
         }
         
         function insertPoint(path, width, height) {
-            var newpath = path.slice();
-            var position = Math.floor(Math.random() * (path.length - 1)) + 1;
-            var newpoint = { x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height) };
+            const newpath = path.slice();
+            const position = Math.floor(Math.random() * (path.length - 1)) + 1;
+            const newpoint = { x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height) };
+            
             newpath.splice(position, 0, newpoint);
+            
             return newpath;
         }
         
         function movePoint(path, width, height) {
-            var newpath = path.slice();
-            var position = Math.floor(Math.random() * (path.length - 1)) + 1;
+            const newpath = path.slice();
+            const position = Math.floor(Math.random() * (path.length - 1)) + 1;
+            
             if (position == path.length - 1)
                 return newpath;
-            var point = path[position];
-            var dx = Math.floor(Math.random() * 5) - 2;
-            var dy = Math.floor(Math.random() * 5) - 2;
-            var newx = point.x + dx;
-            var newy = point.y + dy;
+            
+            const point = path[position];
+            const dx = Math.floor(Math.random() * 5) - 2;
+            const dy = Math.floor(Math.random() * 5) - 2;
+            let newx = point.x + dx;
+            let newy = point.y + dy;
             
             if (newx < 0)
                 newx = 0;
@@ -49,31 +53,36 @@ var Path = (function () {
             if (newy >= height)
                 newy = height - 1;
                 
-            var newpoint = { x: newx, y: newy };
+            const newpoint = { x: newx, y: newy };
             newpath[position] = newpoint;
             
             return newpath;
         }
         
         function removePoint(path) {
-            var newpath = path.slice();
-            var position = Math.floor(Math.random() * (path.length - 2)) + 1;
+            const newpath = path.slice();
+            const position = Math.floor(Math.random() * (path.length - 2)) + 1;
+            
             if (position == path.length - 1)
                 return newpath;
+            
             newpath.splice(position, 1);
+            
             return newpath;
         }
     }
     
     function Genotype(world, path) {
-        var evaluated = false;
-        var stonevalue = Math.sqrt(world.width() * world.width() + world.height() * world.height());
-        var value;
-        var npoints = path.length;
+        let evaluated = false;
+        const stonevalue = Math.sqrt(world.width() * world.width() + world.height() * world.height());
+        let value;
+        let npoints = path.length;
         
-        this.path = function () { return path; }
+        this.path = function () { return path; };
         
-        this.world = function () { return world; }
+        this.world = function () { return world; };
+
+        this.value = function () { return value; };
         
         this.evaluate = function () {
             if (evaluated)
@@ -81,13 +90,13 @@ var Path = (function () {
                 
             value = 0;
             
-            for (var k = 0; k < npoints - 1; k++) {
-                var from = path[k];
-                var to = path[k + 1];
+            for (let k = 0; k < npoints - 1; k++) {
+                const from = path[k];
+                const to = path[k + 1];
                 
                 value += Math.sqrt((from.x - to.x) * (from.x - to.x) + (from.y - to.y) * (from.y - to.y));
                 
-                var stones = world.stones(from, to);
+                const stones = world.stones(from, to);
                 
                 value += stones.length * stonevalue;
             }
@@ -99,23 +108,23 @@ var Path = (function () {
     }
 
     function World(width, height) {
-        var values = [];
+        const values = [];
         
-        for (var x = 0; x < width; x++)
-            for (var y = 0; y < height; y++)
+        for (let x = 0; x < width; x++)
+            for (let y = 0; y < height; y++)
                 values[y * width + x] = false;
         
-        this.width = function () { return width; }
+        this.width = function () { return width; };
         
-        this.height = function () { return height; }
+        this.height = function () { return height; };
         
-        this.get = function (x, y) { return values[y * width + x]; }
+        this.get = function (x, y) { return values[y * width + x]; };
         
-        this.set = function (x, y, value) { values[y * width + x] = value; }
+        this.set = function (x, y, value) { values[y * width + x] = value; };
         
         this.fill = function (ratio) {
-            for (var x = 0; x < width; x++)
-                for (var y = 0; y < height; y++)
+            for (let x = 0; x < width; x++)
+                for (let y = 0; y < height; y++)
                     values[y * width + x] = Math.random() <= ratio;
         }
 
@@ -123,20 +132,20 @@ var Path = (function () {
             var points = [];
             
             if (from.x > to.x || (from.x == to.x && from.y > to.y)) {
-                var temp = from;
+                const temp = from;
                 from = to;
                 to = temp;                
             }
             
             if (from.y == to.y) {
-                for (var x = from.x; x <= to.x; x++)
+                for (let x = from.x; x <= to.x; x++)
                     points.push({ x: x, y: to.y });
                         
                 return points;
             }
             
             if (from.x == to.x) {
-                for (var y = from.y; y <= to.y; y++)
+                for (let y = from.y; y <= to.y; y++)
                     points.push({ x: to.x, y: y });
                         
                 return points;
@@ -150,10 +159,11 @@ var Path = (function () {
             var toy = to.y + dy;
             var fromx = from.x;
             
-            for (var y = from.y; y != toy; y += dy) {
-                var npoints = 0;
-                for (var x = fromx; x <= to.x; x++) {
-                    var point = { x: x, y: y };
+            for (let y = from.y; y != toy; y += dy) {
+                let npoints = 0;
+                
+                for (let x = fromx; x <= to.x; x++) {
+                    const point = { x: x, y: y };
                     
                     if (distance(from, to, point) <= 0.5) {
                         if (npoints == 0)
@@ -170,9 +180,9 @@ var Path = (function () {
         }
         
         this.stones = function (from, to) {
-            var stones = [];
-            var points = this.points(from, to);
-            var npoints = points.length;
+            const stones = [];
+            const points = this.points(from, to);
+            const npoints = points.length;
             
             for (var k = 0; k < npoints; k++)
                 if (this.get(points[k].x, points[k].y))
@@ -184,12 +194,12 @@ var Path = (function () {
     
     // http://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
     function distance(from, to, point) {
-        var dx = to.x - from.x;
-        var dy = to.y - from.y;
+        const dx = to.x - from.x;
+        const dy = to.y - from.y;
         
-        var a = -dy;
-        var b = dx;
-        var c = dy * from.x - dx * from.y;
+        const a = -dy;
+        const b = dx;
+        const c = dy * from.x - dx * from.y;
         
         return Math.abs(a * point.x + b * point.y + c) / Math.sqrt(a * a + b * b);
     }
@@ -198,11 +208,11 @@ var Path = (function () {
         if (!ratio)
             ratio = 0.5;
             
-        var points = [from, to];
+        const points = [from, to];
         
         while (Math.random() <= ratio) {
-            var position = Math.floor(Math.random() * (points.length - 1)) + 1;
-            var newpoint = { x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height) };
+            const position = Math.floor(Math.random() * (points.length - 1)) + 1;
+            const newpoint = { x: Math.floor(Math.random() * width), y: Math.floor(Math.random() * height) };
             points.splice(position, 0, newpoint);
         }
         
@@ -210,7 +220,8 @@ var Path = (function () {
     }
     
     function createGenotype(world, from, to, ratio) {
-        var path = createPath(from, to, world.width(), world.height(), ratio);
+        const path = createPath(from, to, world.width(), world.height(), ratio);
+        
         return new Genotype(world, path);
     }
     
